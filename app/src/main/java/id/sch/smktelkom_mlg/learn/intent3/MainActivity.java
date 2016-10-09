33ca3be82110ec2f68d25093e15ace9e5cb186b7
Krisna Putra F <contact.krisnapf@gmail.com>
2016-10-09 13:19:26 +0700

    Intent Browser

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -14,11 +14,17 @@ public class MainActivity extends AppCompatActivity {
         if (intent.resolveActivity(getPackageManager()) != null)
             startActivity(intent);
     }
-
     public void composeSmsMessage(String message) {
         Intent intent = new Intent(Intent.ACTION_SEND);
         intent.setType("text/plain");
         intent.putExtra("sms_body", message);
+        if (intent.resolveActivity(getPackageManager()) != null)
+            startActivity(intent);
+    }
+
+    public void openWebPage(String url) {
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
         if (intent.resolveActivity(getPackageManager()) != null)
             startActivity(intent);
     }
@@ -41,6 +47,14 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onClick(View v) {
                         composeSmsMessage("Pesan dari SMK Telkom Malang");
+                    }
+                });
+
+        findViewById(R.id.imageViewBrowser)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        openWebPage("http://www.smktelkom-mlg.sch.id/");
                     }
                 });
     }
