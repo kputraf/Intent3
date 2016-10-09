82cc024af7f589bce7ef8cd9efd1e1f588bcbcb2
Krisna Putra F <contact.krisnapf@gmail.com>
2016-10-09 13:13:24 +0700

    Intent SMS

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
     public void dialPhoneNumber(String phoneNumber) {
         Intent intent = new Intent(Intent.ACTION_DIAL);
         intent.setData(Uri.parse("tel:" + phoneNumber));
@@ -14,6 +15,13 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
     }
 
+    public void composeSmsMessage(String message) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra("sms_body", message);
+        if (intent.resolveActivity(getPackageManager()) != null)
+            startActivity(intent);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,5 +35,13 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
+
+        findViewById(R.id.imageViewSMS)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        composeSmsMessage("Pesan dari SMK Telkom Malang");
+                    }
+                });
     }
 }
